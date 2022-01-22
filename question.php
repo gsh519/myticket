a24763efce241889d53da989eb7d94f72ec556be
gsh519 <gsh05196yu@yahoo.co.jp>
2022-01-22 21:10:28 +0900

    content edit

--- question.php
+++ question.php
@@ -48,73 +48,121 @@
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>1</span>問目</p>
-          <h2 class="question-subject">苦手な果物</h2>
+          <p class="question-number"><span>2</span>問目</p>
+          <h2 class="question-subject">悠斗と芽衣が付き合った日にちはいつでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>11月12日</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>10月12日</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>12月13日</p>
             </label>
           </div>
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>1</span>問目</p>
-          <h2 class="question-subject">苦手な果物</h2>
+          <p class="question-number"><span>3</span>問目</p>
+          <h2 class="question-subject">1年記念日で行った旅行先はどこでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>淡路島</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>京都</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>大阪空庭温泉</p>
             </label>
           </div>
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>1</span>問目</p>
-          <h2 class="question-subject">苦手な果物</h2>
+          <p class="question-number"><span>4</span>問目</p>
+          <h2 class="question-subject">悠斗は何人家族でしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>3人家族</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>5人家族</p>
             </label>
           </div>
           <div class="select-area">
             <label for="answer_1">
               <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
+              <p>4人家族</p>
+            </label>
+          </div>
+        </li>
+
+        <li class="questions__item">
+          <p class="question-number"><span>5</span>問目</p>
+          <h2 class="question-subject">悠斗と芽衣はどこで出会ったでしょう？</h2>
+          <p class="question-comm">正解だと思うものを１つ選んでください</p>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>ナンパ</p>
+            </label>
+          </div>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>友人の紹介</p>
+            </label>
+          </div>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>映画館のバイト</p>
+            </label>
+          </div>
+        </li>
+
+        <li class="questions__item">
+          <p class="question-number"><span>6</span>問目</p>
+          <h2 class="question-subject">初めて行った旅行先はどこでしょう？</h2>
+          <p class="question-comm">正解だと思うものを１つ選んでください</p>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>京都旅行</p>
+            </label>
+          </div>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>長島スパーランド</p>
+            </label>
+          </div>
+          <div class="select-area">
+            <label for="answer_1">
+              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
+              <p>東京</p>
             </label>
           </div>
         </li>
@@ -124,20 +172,16 @@
     </div>
 
     <div class="answer">
-      <h2>10問中 <span>5問正解！</span></h2>
+      <h2>6問中 <span>6問正解！</span></h2>
       <ul class="answer__list">
         <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
-        <li>1問目：◯</li>
+        <li>2問目：◯</li>
+        <li>3問目：◯</li>
+        <li>4問目：◯</li>
+        <li>5問目：◯</li>
+        <li>6問目：◯</li>
       </ul>
-      <p>おめでとうございます！<br>チケットを受け取ることができます！！</p>
+      <p class="answer__comm">おめでとうございます！<br>チケットを受け取ることができます！！</p>
       <a href="./ticket.php" class="ticket-get"><i class="fas fa-gift"></i>チケットをもらう！！</a>
     </div>
   </div>
