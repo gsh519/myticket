023029993acac98042f3a1908659e01bd2f88661
gsh519 <gsh05196yu@yahoo.co.jp>
2022-01-30 19:35:25 +0900

    クイズをdbから表示させる

--- get.php
+++ get.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once("./function.php");
+
 //初期値
 $errors = [];
 
@@ -9,22 +11,6 @@ $submit = $_GET['submit'];
 if (!empty($submit)) {
   if (empty($ticket_id)) {
     $errors[] = 'チケットIDを入力してください';
-  }
-
-  // データーベース接続関数
-  function dbConnect($db_name, $db_host, $db_user, $db_pass)
-  {
-    // データーベースに接続
-    try {
-      $option = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::MYSQL_ATTR_MULTI_STATEMENTS => false,
-      ];
-      $pdo = new PDO("mysql:charset=UTF8;dbname=$db_name;host=$db_host", $db_user, $db_pass, $option);
-      return $pdo;
-    } catch (PDOException $e) {
-      $errors[] = $e->getMessage();
-    }
   }
 
   $pdo = dbConnect("myticket", "localhost", "root", "root");
--- question.php
+++ question.php
@@ -1,3 +1,40 @@
+<?php
+require_once("function.php");
+$errors = [];
+$ticket_id = $_GET['ticket_id'];
+if (!empty($ticket_id)) {
+  //db接続
+  $pdo = dbConnect("myticket", "localhost", "root", "root");
+
+  //sql文作成
+  $sql = "SELECT * FROM question WHERE ticket_id = :ticket_id";
+
+  $stmt = $pdo->prepare($sql);
+
+  //値のセット
+  $stmt->bindParam(":ticket_id", $ticket_id, PDO::PARAM_STR);
+
+  //実行
+  $stmt->execute();
+
+  //データの取得
+  $questions = $stmt->fetchAll();
+
+  $stmt = null;
+  $pdo = null;
+} else {
+  $errors[] = 'チケットIDを取得できません';
+}
+
+if ($_POST) {
+  while ($number < count($questions)) {
+    $number += 1;
+
+    $_POST['answer' . $number];
+  }
+}
+
+?>
 <!DOCTYPE html>
 <html lang="ja">
 
@@ -20,35 +57,39 @@
     </div>
 
     <p class="question-ttl">クイズを解いてチケットをもらおう!</p>
-    <div class="questions">
-      <ul>
+    <form action="" method="POST">
+      <div class="questions">
+        <ul>
+          <?php if (!empty($questions)) : ?>
+            <?php foreach ($questions as $number => $question) : ?>
+              <li class="questions__item">
+                <p class="question-number"><span><?php echo $number + 1; ?></span>問目</p>
+                <h2 class="question-subject"><?php echo $question['subject']; ?></h2>
+                <p class="question-comm">正解だと思うものを１つ選んでください</p>
+                <div class="select-area">
+                  <label for="answer_1<?php echo $number; ?>">
+                    <input type="radio" class="select-box" name="answer<?php echo $number; ?>" value="<?php echo $question['answer_list1']; ?>" id="answer_1<?php echo $number; ?>">
+                    <p><?php echo $question['answer_list1']; ?></p>
+                  </label>
+                </div>
+                <div class="select-area">
+                  <label for="answer_2<?php echo $number; ?>">
+                    <input type="radio" class="select-box" name="answer<?php echo $number; ?>" value="<?php echo $question['answer_list2']; ?>" id="answer_2<?php echo $number; ?>">
+                    <p><?php echo $question['answer_list2']; ?></p>
+                  </label>
+                </div>
+                <div class="select-area">
+                  <label for="answer_3<?php echo $number; ?>">
+                    <input type="radio" class="select-box" name="answer<?php echo $number; ?>" value="<?php echo $question['answer_list3']; ?>" id="answer_3<?php echo $number; ?>">
+                    <p><?php echo $question['answer_list3']; ?></p>
+                  </label>
+                </div>
+              </li>
+            <?php endforeach; ?>
+          <?php endif; ?>
 
-        <li class="questions__item">
+          <!-- <li class="questions__item">
           <p class="question-number"><span>1</span>問目</p>
-          <h2 class="question-subject">苦手な果物</h2>
-          <p class="question-comm">正解だと思うものを１つ選んでください</p>
-          <div class="select-area">
-            <label for="answer_1">
-              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
-            </label>
-          </div>
-          <div class="select-area">
-            <label for="answer_1">
-              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
-            </label>
-          </div>
-          <div class="select-area">
-            <label for="answer_1">
-              <input type="radio" class="select-box" name="answer_1" value="いちご" id="answer_1">
-              <p>いちご</p>
-            </label>
-          </div>
-        </li>
-
-        <li class="questions__item">
-          <p class="question-number"><span>2</span>問目</p>
           <h2 class="question-subject">悠斗と芽衣が付き合った日にちはいつでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
@@ -72,7 +113,7 @@
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>3</span>問目</p>
+          <p class="question-number"><span>2</span>問目</p>
           <h2 class="question-subject">1年記念日で行った旅行先はどこでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
@@ -96,7 +137,7 @@
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>4</span>問目</p>
+          <p class="question-number"><span>3</span>問目</p>
           <h2 class="question-subject">悠斗は何人家族でしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
@@ -120,7 +161,7 @@
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>5</span>問目</p>
+          <p class="question-number"><span>4</span>問目</p>
           <h2 class="question-subject">悠斗と芽衣はどこで出会ったでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
@@ -144,7 +185,7 @@
         </li>
 
         <li class="questions__item">
-          <p class="question-number"><span>6</span>問目</p>
+          <p class="question-number"><span>5</span>問目</p>
           <h2 class="question-subject">初めて行った旅行先はどこでしょう？</h2>
           <p class="question-comm">正解だと思うものを１つ選んでください</p>
           <div class="select-area">
@@ -165,14 +206,15 @@
               <p>東京</p>
             </label>
           </div>
-        </li>
+        </li> -->
 
-        <input type="submit" name="check" value="採点する" class="check">
-      </ul>
-    </div>
+          <input type="submit" name="check" value="採点する" class="check">
+        </ul>
+      </div>
+    </form>
 
-    <div class="answer">
-      <h2>6問中 <span>6問正解！</span></h2>
+    <!-- <div class="answer">
+      <h2>5問中 <span>5問正解！</span></h2>
       <ul class="answer__list">
         <li>1問目：◯</li>
         <li>2問目：◯</li>
@@ -183,7 +225,7 @@
       </ul>
       <p class="answer__comm">おめでとうございます！<br>チケットを受け取ることができます！！</p>
       <a href="./ticket.php" class="ticket-get"><i class="fas fa-gift"></i>チケットをもらう！！</a>
-    </div>
+    </div> -->
   </div>
 </body>
 
